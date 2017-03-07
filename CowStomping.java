c673af3f3c3c3db705a8ac0d11024f87476a0008
Jake Goldman <jgoldman@stuy.edu>
2017-03-07 12:29:58 -0500

    Converted to static, finished loading stuff

--- CowStomping.java
+++ CowStomping.java
@@ -1,9 +1,76 @@
 import java.util.*;
 import java.io.*;
+
 public class CowStomping{
+
+
+
+    public static int bronze(String filename){
+	// Read the file
+	Scanner sc = new Scanner(new File(filename) );
+
+	// For getting the metadata from the first line
+	String firstLine = sc.nextLine();
+	Scanner firstLineScan = new Scanner( firstLine );
+	int rows = firstLineScan.next();
+	System.out.println("Rows are: " + rows);
+	int cols = firstLineScan.next();
+	System.out.println("Cols are: " + cols);
+	int elevation = firstLineScan.next();
+	System.out.println("Elevation is: " + elevation);
+	int numOrders = firstLineScan.next();
+
+	// Creating the pasture/order array
+	int[][]pasture = new int[rows][cols];
+	int[][]orders = new int[numOrders][3];
+
+	// Loading the pasture
+	for (int r=0; r < rows; r++){
+	    Scanner lineScan = new Scanner(sc.nextLine());
+	    for (int c=0; c<cols; c++){
+		pasture[r][c]=lineScan.next();
+	    }
+	}
+	System.out.println(arrayToString(pasture));
+
+
+	// Loading the orders
+	for (int i = 0; i < numOrders; i++){
+	    Scanner orderScan = new Scanner(sc.nextLine());
+	    for (int o = 0; o < 3; o++){
+		orders[i][o] = orderScan.next();
+	    }
+	}
+	System.out.println(arrayToString(orders));
+	
+
+	return 0; // Change later
+    }
+
+
+    private static String arrayToString(int[][]ary){
+	String ans = "";
+	for(int row = 0; row < ary.length; row++){
+	    for(int col = 0; col < ary[row].length; col++){
+		if(ary[row][col] < 10){
+		    ans += "  " + ary[row][col];
+		} else {
+		    ans += " " + ary[row][col];
+		}
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
+
+
+
+    /*
     int [][] pasture;
     int [][] order;
     int  elevation;
+
+
     public CowStomping(String file) {
     try{
         Scanner sc = new Scanner(new File(file));
@@ -19,7 +86,7 @@ public class CowStomping{
         //Loading pasture
         for (int i=0; i < r; i++){
         // Visits every int
-        Scanner lineScan = new Scanner(sc.nextLiyne());
+        Scanner lineScan = new Scanner(sc.nextLine());
         for (int e=0; e<c; e++){
             pasture[i][e]=lineScan.next();
         }
@@ -27,9 +94,9 @@ public class CowStomping{
         //Loading Orders
 
     }
-    /*catch{
+    catch{
         
     }
+    
     */
-    } 
 }
